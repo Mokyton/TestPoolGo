c4740df572d1e9f4620c830f8ea99e02d7711b8e
Chillwave Strong <chillwav@un-s2.kzn.21-school.ru>
2023-01-21 22:17:52 +0300

    DAY03 ex01 getPlaces done

--- Go_Day03-0/src/ex01/pkg/db/getPlaces.go
+++ Go_Day03-0/src/ex01/pkg/db/getPlaces.go
@@ -1,14 +1,16 @@
 package db
 
+import (
+	"context"
+	"encoding/json"
+	"github.com/elastic/go-elasticsearch/v7"
+	"github.com/elastic/go-elasticsearch/v7/esapi"
+)
+
 type Place struct {
-	ID       int    `json:"id"`
-	Name     string `json:"name"`
-	Address  string `json:"address"`
-	Phone    string `json:"phone"`
-	Location struct {
-		Lat float64 `json:"lat"`
-		Lon float64 `json:"lon"`
-	} `json:"location"`
+	Name    string `json:"name"`
+	Address string `json:"address"`
+	Phone   string `json:"phone"`
 }
 
 type types struct {
@@ -16,5 +18,62 @@ type types struct {
 }
 
 func GetPlaces(limit int, offset int) ([]Place, int, error) {
+	data := types{}
+	es, err := elasticsearch.NewDefaultClient()
+	if err != nil {
+		return nil, 0, err
+	}
+	req := esapi.SearchRequest{
+		Index:        []string{"places"},
+		DocumentType: []string{"place"},
+		Size:         &limit,
+		From:         &offset,
+	}
 
+	res, err := req.Do(context.Background(), es)
+	if err != nil {
+		return nil, 0, err
+	}
+
+	if err := json.NewDecoder(res.Body).Decode(&data); err != nil {
+		return nil, 0, err
+	}
+
+	res, err = es.Count()
+	if err != nil {
+		return nil, 0, err
+	}
+
+	total := struct {
+		Total int `json:"count"`
+	}{}
+	if err := json.NewDecoder(res.Body).Decode(&total); err != nil {
+		return nil, 0, err
+	}
+
+	return data.Places, total.Total, nil
+}
+
+func (p *types) UnmarshalJSON(data []byte) error {
+	tmpl := struct {
+		Hits struct {
+			Hits []struct {
+				Source struct {
+					Name    string `json:"name"`
+					Address string `json:"address"`
+					Phone   string `json:"phone"`
+				} `json:"_source"`
+			} `json:"hits"`
+		} `json:"hits"`
+	}{}
+
+	if err := json.Unmarshal(data, &tmpl); err != nil {
+		return err
+	}
+
+	for _, v := range tmpl.Hits.Hits {
+		p.Places = append(p.Places, v.Source)
+	}
+
+	return nil
 }
--- Go_Day03-0/src/ex01/pkg/types.go
+++ Go_Day03-0/src/ex01/pkg/types.go
@@ -1,7 +1,6 @@
 package pkg
 
 type Store interface {
-	// returns a list of items, a total number of hits and (or) an error in case of one
 	GetPlaces(limit int, offset int) ([]Place, int, error)
 }
 

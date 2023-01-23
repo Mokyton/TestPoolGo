6350f31ff5787a27fee97c1cf26199268eff1a4d
Chillwave Strong <chillwav@un-s2.kzn.21-school.ru>
2023-01-23 13:22:52 +0300

    DAY03 ex02 done

--- Go_Day03-0/src/ex02/db/getPlaces.go
+++ Go_Day03-0/src/ex02/db/getPlaces.go
@@ -1,1 +1,105 @@
 package db
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"github.com/elastic/go-elasticsearch/v7"
+	"github.com/elastic/go-elasticsearch/v7/esapi"
+	"github.com/olivere/elastic"
+	"time"
+)
+
+type Place struct {
+	ID       int              `json:"id"`
+	Name     string           `json:"name"`
+	Address  string           `json:"address"`
+	Phone    string           `json:"phone"`
+	Location elastic.GeoPoint `json:"location"`
+}
+
+type Types struct {
+	Name     string  `json:"_index"`
+	ScrollID string  `json:"_scroll_id"`
+	Places   []Place `json:"_source"`
+}
+
+func New() *Types {
+	places := make([]Place, 0, 10)
+	return &Types{Places: places}
+}
+
+func (p *Types) GetPlaces(limit int, offset int) ([]Place, int, error) {
+	es, err := elasticsearch.NewDefaultClient()
+	if err != nil {
+		return nil, 0, err
+	}
+	req := esapi.SearchRequest{
+		Index:        []string{"places"},
+		DocumentType: []string{"place"},
+		Size:         &limit,
+		Scroll:       1 * time.Minute,
+	}
+
+	res, err := req.Do(context.Background(), es)
+	if err != nil {
+		return nil, 0, err
+	}
+
+	if err := json.NewDecoder(res.Body).Decode(&p); err != nil {
+		return nil, 0, err
+	}
+
+	res, err = es.Count()
+	if err != nil {
+		return nil, 0, err
+	}
+
+	total := struct {
+		Total int `json:"count"`
+	}{}
+	if err := json.NewDecoder(res.Body).Decode(&total); err != nil {
+		return nil, 0, err
+	}
+
+	if total.Total <= (offset-1)*10 {
+		return nil, total.Total, errors.New("the page doesn't exists")
+	}
+
+	for i := 1; i < offset; i++ {
+		res, err := es.Scroll(es.Scroll.WithScrollID(p.ScrollID), es.Scroll.WithScroll(time.Minute))
+		if err = json.NewDecoder(res.Body).Decode(&p); err != nil {
+			return nil, 0, err
+		}
+	}
+
+	return p.Places, total.Total, nil
+}
+
+func (p *Types) UnmarshalJSON(data []byte) error {
+	p.Places = p.Places[:0]
+	tmpl := struct {
+		ScrollID string `json:"_scroll_id"`
+		Hits     struct {
+			Hits []struct {
+				Source struct {
+					ID       int              `json:"id"`
+					Name     string           `json:"name"`
+					Address  string           `json:"address"`
+					Phone    string           `json:"phone"`
+					Location elastic.GeoPoint `json:"location"`
+				} `json:"_source"`
+			} `json:"hits"`
+		} `json:"hits"`
+	}{}
+
+	if err := json.Unmarshal(data, &tmpl); err != nil {
+		return err
+	}
+	p.ScrollID = tmpl.ScrollID
+	for _, v := range tmpl.Hits.Hits {
+		p.Places = append(p.Places, v.Source)
+	}
+
+	return nil
+}

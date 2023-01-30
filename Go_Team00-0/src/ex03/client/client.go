11536e71bb578c347416239a0c622bd22866a1e4
Chillwave Strong <chillwav@un-s2.kzn.21-school.ru>
2023-01-30 18:17:21 +0300

    almost done?

--- Go_Team00-0/src/ex03/client/client.go
+++ Go_Team00-0/src/ex03/client/client.go
@@ -2,10 +2,20 @@ package main
 
 import (
 	"context"
-	pb "ex00/gen/proto"
+	pb "ex03/gen/proto"
+	"ex03/psql"
 	"fmt"
+	_ "github.com/lib/pq"
 	"google.golang.org/grpc"
 	"log"
+)
+
+const (
+	host     = "localhost"
+	port     = "5432"
+	user     = "chillwavuser"
+	password = "pgpwd4chillwav"
+	dbname   = "chillwavdb"
 )
 
 func main() {
@@ -13,7 +23,14 @@ func main() {
 	if err != nil {
 		log.Fatalln(err)
 	}
-
+	db, err := psql.NewDbConnection(host, port, user, password, dbname)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	err = db.CreateTable()
+	if err != nil {
+		log.Fatalln(err)
+	}
 	//data := make([]float64, 0, 100([]float64, 0, 100)
 	//	var)
 	var summFreq float64
@@ -30,6 +47,10 @@ func main() {
 		fmt.Println(resp.Frequency, resp.Frequency-summFreq/float64(i), summFreq/float64(i), summstd/float64(i))
 		i++
 		if i == 50 {
+			err = db.Insert(resp.SessionId, resp.Frequency, resp.UTC)
+			if err != nil {
+				log.Fatalln(err)
+			}
 			break
 		}
 	}
---
+++ Go_Team00-0/src/ex03/server/server.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"context"
+	pb "ex03/gen/proto"
+	"fmt"
+	uuid2 "github.com/satori/go.uuid"
+	"google.golang.org/grpc"
+	"google.golang.org/protobuf/types/known/timestamppb"
+	"log"
+	"math/rand"
+	"net"
+	"os"
+)
+
+type TransmitterApiServer struct {
+	pb.UnimplementedTransmitterApiServer
+}
+
+func (s *TransmitterApiServer) Connection(ctx context.Context, req *pb.Request) (*pb.Response, error) {
+	res := &pb.Response{}
+	res.SessionId = uuid2.NewV4().String()
+	std := rand.Float64()*(1.5-0.3+1) + 0.3
+	mean := rand.Float64()*(10+10+1) - 10
+	res.Frequency = rand.NormFloat64()*std + mean
+
+	res.UTC = timestamppb.Now()
+	_, err := fmt.Fprintln(os.Stdout, res.UTC.AsTime(), res.SessionId, res.Frequency, mean, std)
+	if err != nil {
+		return nil, err
+	}
+	return res, nil
+}
+
+func main() {
+	listner, err := net.Listen("tcp", "localhost:8080")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	grpcServer := grpc.NewServer()
+
+	pb.RegisterTransmitterApiServer(grpcServer, &TransmitterApiServer{})
+
+	err = grpcServer.Serve(listner)
+	if err != nil {
+		log.Println(err)
+	}
+}

c7f06a4c791c2ea81ab2dcef75d36dd92bd1645b
Chillwave Strong <chillwav@ge-n5.kzn.21-school.ru>
2023-04-04 15:10:23 +0300

    DAY06 ex01 is done

--- Go_Day06-0/src/ex01/cmd/web/handlers.go
+++ Go_Day06-0/src/ex01/cmd/web/handlers.go
@@ -1,30 +1,98 @@
 package main
 
 import (
+	"errors"
+	"fmt"
 	"html/template"
 	"net/http"
+	"strconv"
+	"strings"
 )
 
-func (app *application) adminHandler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/admin" {
+func (app *application) loginHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/login" {
 		http.NotFound(w, r)
 		return
 	}
+	r.ParseForm()
+	if r.Form.Get("login") == "admin" && r.Form.Get("password") == "admin" {
+		ses, err := cookieStore.Get(r, cookieName)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
 
-	t, err := template.ParseFiles("./ui/html/login.gohtml")
+		ses.Values[sesKeyLogin] = "admin"
+		err = cookieStore.Save(r, w, ses)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+	}
+	http.Redirect(w, r, "/", http.StatusPermanentRedirect)
+}
 
+func (app *application) signIn(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("./ui/html/signin.gohtml")
 	if err != nil {
-		http.Error(w, err.Error(), 555)
+		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 	t.Execute(w, nil)
-
 }
-func (app *application) homeHandler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		http.NotFound(w, r)
+
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
+	ses, err := cookieStore.Get(r, cookieName)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
-	w.Write([]byte("Hello world"))
+	login, ok := ses.Values[sesKeyLogin].(string)
+	if !ok {
+		login = "anonymous"
+	}
 
+	var page, nextPage, prevPage int
+	var limit = 3
+
+	if strings.HasPrefix(r.URL.RawQuery, "page=") {
+		num, err := strconv.Atoi(strings.TrimPrefix(r.URL.RawQuery, "page="))
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		page = num
+	} else {
+		page = 1
+	}
+
+	total := app.thoughts.GetCountOfThoughts()
+	thoughts, _ := app.thoughts.GetThoughts(limit, limit*(page-1))
+	if page*limit < total {
+		nextPage = page + 1
+	} else {
+		nextPage = 0
+	}
+	prevPage = page - 1
+
+	user, ok := users[login]
+	if !ok {
+		user = User{Name: login, FullAccess: false}
+	}
+	user.Thoughts = thoughts
+	user.Previous = prevPage
+	user.Next = nextPage
+	t, err := template.ParseFiles("./ui/html/home.gohtml")
+	t.Execute(w, user)
+}
+
+func (app *application) CreateThought(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/createThought" || r.Method != "POST" {
+		http.Error(w, errors.New("Bad request ").Error(), http.StatusBadRequest)
+		return
+	}
+	r.ParseForm()
+	fmt.Println(r.Form.Get("Title"), r.Form.Get("Content"))
+	app.thoughts.Insert(r.Form.Get("Title"), r.Form.Get("Content"))
+	http.Redirect(w, r, "/", http.StatusPermanentRedirect)
 }
--- Go_Day06-0/src/ex01/cmd/web/main.go
+++ Go_Day06-0/src/ex01/cmd/web/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"encoding/gob"
+	"ex01/pkg/db/psql"
 	"flag"
+	_ "github.com/lib/pq"
 	"log"
 	"net/http"
 	"os"
@@ -10,26 +13,37 @@ import (
 type application struct {
 	errorLog *log.Logger
 	infoLog  *log.Logger
+	thoughts *psql.Model
 }
 
 func main() {
 	addr := flag.String("addr", ":8888", "Сетевой адрес HTTP")
-
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	db, err := psql.NewDbConnection(host, port, userdb, password, dbname)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+	err = db.CreateTable()
+	if err != nil {
+		errorLog.Fatal(err)
+	}
 
 	app := &application{
 		errorLog: errorLog,
 		infoLog:  infoLog,
+		thoughts: db,
 	}
+
+	gob.Register(sesKey(0))
 
 	srv := &http.Server{
 		Addr:     *addr,
+		Handler:  app.Router(),
 		ErrorLog: errorLog,
-		Handler:  app.routes(),
 	}
 
 	infoLog.Printf("Запуск сервера на %s\n", *addr)
-	err := srv.ListenAndServe()
-	errorLog.Fatal(err)
+	srv.ListenAndServe()
 }
